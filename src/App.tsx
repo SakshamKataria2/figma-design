f951503e17e77002470180507a6cb9ea40d64efd
sakshamkataria <sakshamkataria@outlook.com>
2024-06-05 13:14:41 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import { Signin } from "./pages/Signin.tsx";
+import Onboard from "./pages/Onboard.tsx";
+import Menu from "./pages/Menu.tsx";
+import { Friends } from "./pages/Friends.tsx";
+import { Message } from "./pages/Message.tsx";
+import { Profile } from "./pages/Profile.tsx";
+import { Notification } from "./pages/Notification.tsx";
+
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Outlet />}>
+        <Route path="/" element={<Onboard />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/notification" element={<Notification />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/messages" element={<Message />} />
+      </Route>
+    )
+  );
+
+  return (
+    <div className="w-screen h-screen">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
